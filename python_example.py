9ca6a6abc78173a04e9fb590738e6f14f6311f46
Sabarish Mogallapalli <themogallapalli@gmail.com>
2024-02-23 14:31:28 -0600

    Adding example files

---
+++ python_example.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv('https://data.illinois.gov/dataset/820f5916-069f-41e8-afc2-815a26770bea/resource/dfcad952-67cc-4ff3-8be0-0312c25ec8e1/download/chm_jail_data.csv')
+print(df.head())
